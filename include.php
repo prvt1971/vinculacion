fd37106a42d759d44974d372e4d712f8be3b99f6
Pedro Roberto Valdés Tamayo <pvaldestamatamayo@gmail.com>
2024-07-02 10:59:39 -0500

    Añadi parametros para la conexión al servidor de https://console.clever-cloud.com/

--- include.php
+++ include.php
@@ -1,13 +1,15 @@
 <?php
-    $USER = "root";
-    // $USER = "prvtonen_pvaldestamayo"; //Bluehost
-    $PASSWORD = "";
-    //$PASSWORD = "Cleopatra71*/MySql"; //bluehost
-    $HOST="localhost";
-    //$HOST="50.87.220.245"; //bluehost
+    //Configuración para el servidor local
+    //$USER = "root";
+    //$PASSWORD = "";
+    //$HOST="localhost";
+    //$PATH = "H:/Mi unidad/www/titulacion";
+    //$W3DIRECTORY = "http://localhost/titulacion";
+    //$DBNAME = "titulacion";
+    //Configuración para el servidor remoto
+    $USER = "utyijjes9h2sgq74"; //https://console.clever-cloud.com/
+    $PASSWORD = "q9k1cV8j0potsmhD1F8I"; //https://console.clever-cloud.com/
+    $HOST="bzwlcnn8eil2ducltnxb-mysql.services.clever-cloud.com"; //https://console.clever-cloud.com/
     $PATH = "H:/Mi unidad/www/titulacion";
-    //$PATH = "C:/xampp/htdocs/titulacion";
-    $W3DIRECTORY = "http://localhost/titulacion";
-    $DBNAME = "titulacion";
-    //$DBNAME = "prvtonen_titulacion"; //bluehost
+    $DBNAME = "bzwlcnn8eil2ducltnxb"; //https://console.clever-cloud.com/
 ?>
